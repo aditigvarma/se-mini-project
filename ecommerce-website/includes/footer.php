9362f29481b0b7fc721ed1d220d26ae20a933412
aditigvarma <78954407+aditigvarma@users.noreply.github.com>
2022-04-19 13:26:25 +0530

    Update footer.php

--- ecommerce-website/includes/footer.php
+++ ecommerce-website/includes/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
-    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Infi Mart | All Rights Reserved | Contact Us: +91 9876543210</b></span></div>
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Infi Mart | All Rights Reserved | Contact Us: +91 9543217843 | help@infimart.com</b></span></div>
 </footer>
     
